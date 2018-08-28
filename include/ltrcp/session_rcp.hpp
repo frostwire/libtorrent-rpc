f19157e3c8216aa1ade533020713b64797d85fe4
Alden Torres <aldenml@gmail.com>
2018-08-28 08:45:55 -0400

    more on cmake project and added session_rcp class

---
+++ include/ltrcp/session_rcp.hpp
@@ -0,0 +1,19 @@
+/*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Copyright (c) 2018, FrostWire(R). All rights reserved.
+ */
+
+#ifndef LIBTORRENT_RPC_SESSION_RCP_HPP
+#define LIBTORRENT_RPC_SESSION_RCP_HPP
+
+namespace ltrcp
+{
+
+class session_rcp
+{
+
+};
+
+}
+
+#endif // LIBTORRENT_RPC_SESSION_RCP_HPP
---
+++ src/session_rcp.cpp
@@ -0,0 +1,6 @@
+/*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Copyright (c) 2018, FrostWire(R). All rights reserved.
+ */
+
+#include "ltrcp/session_rcp.hpp"
