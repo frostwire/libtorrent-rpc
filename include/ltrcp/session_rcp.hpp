7f30dbf61f9f6b2edd2322a1a5a45b4f6d62dc5d
Alden Torres <aldenml@gmail.com>
2018-08-28 09:16:35 -0400

    actual linking to torrent-rasterbar

--- include/ltrcp/session_rcp.hpp
+++ include/ltrcp/session_rcp.hpp
@@ -6,12 +6,21 @@
 #ifndef LIBTORRENT_RPC_SESSION_RCP_HPP
 #define LIBTORRENT_RPC_SESSION_RCP_HPP
 
+#include <memory>
+
+#include <libtorrent/aux_/disable_warnings_push.hpp>
+#include <libtorrent/session.hpp>
+#include <libtorrent/aux_/disable_warnings_pop.hpp>
+
 namespace ltrcp
 {
 
 class session_rcp
 {
 
+private:
+
+    std::unique_ptr<lt::session> m_session;
 };
 
 }
